2c0455efae2a0bdbeaca12bbd6440ec6f8b6fc31
TiagoBarbosa88 <sombr43@gmail.com>
2022-11-30 22:37:26 -0300

    test jest

---
+++ transactions/model.js
@@ -0,0 +1,29 @@
+mport { TransactionRepository } from "./repository.js";
+
+export class Transaction {
+
+    date;
+    description;
+    money;
+    transactionType;
+    type;
+    user;
+
+    #repository;
+
+    constructor() {
+        this.#repository = new TransactionRepository();
+    }
+
+    findByUser() {
+        if (!this.user?.uid) {
+            return Promise.reject({
+                code: 500,
+                message: "Usuário nao informado"
+            });
+        }
+
+        return this.#repository.findByUserUid(this.user.uid);
+    }
+
+}
